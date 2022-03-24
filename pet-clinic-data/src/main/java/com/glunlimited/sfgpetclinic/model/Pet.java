aa1b6d8083780c1718f46b7a582c75abfe31059b
glunlimited <glunlimited@gmail.com>
2022-03-23 21:10:16 -0400

    Added id as Long property to model objects Closes #10

--- pet-clinic-data/src/main/java/com/glunlimited/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/glunlimited/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.glunlimited.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private LocalDate birthDate;
     private PetType type;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/glunlimited/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/glunlimited/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.glunlimited.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
