a088e24ec7618b3a94c10c21218a27f6aa1e2bcd
glunlimited <glunlimited@gmail.com>
2022-03-24 21:43:55 -0400

    Created pet and owner index page and controller closes #3

--- pet-clinic-web/src/main/java/com/glunlimited/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/glunlimited/sfgpetclinic/controllers/PetController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VetsController {
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
-        return "vets/index";
+public class PetController {
+    @RequestMapping({"/pets","/pets/index","/pets/index.html"})
+    public String listPets(){
+        return "pets/index";
     }
 }
